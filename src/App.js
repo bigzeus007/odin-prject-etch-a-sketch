d546a1958b1a8164583a6216cb7685817a2505fc
said.T <tarhisaid@gmail.com>
2021-06-11 00:25:03 +0100

    REACT
    ODEN FIRST PROJECT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+
+
+
+function MyTabSize(){
+
+const myIndex = 125;
+const myArray = [];
+myArray.length=myIndex*myIndex;
+myArray.fill(true)
+
+
+
+return(
+  <div className="monTab" >
+  {myArray.map(x=> <span style={{width:500/myIndex,height:500/myIndex}} key={x.toString}></span>)
+  }
+  </div>
+);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <MyTabSize  />
+ 
       </header>
     </div>
   );
 }
 
 export default App;
+
