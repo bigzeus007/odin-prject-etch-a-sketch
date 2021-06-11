db2e54f4b29cf473c790bf3cf1ddbc53db1025ca
said.T <tarhisaid@gmail.com>
2021-06-11 19:26:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 
 function MyTabSize(){
 
-const myIndex = 125;
+const myIndex = 6;
 const myArray = [];
 myArray.length=myIndex*myIndex;
-myArray.fill(true)
+myArray.fill(true);
+const myChange=
+const [myChange,setChange]=useState()
 
 
 
 return(
   <div className="monTab" >
-  {myArray.map(x=> <span style={{width:500/myIndex,height:500/myIndex}} key={x.toString}></span>)
+  {myArray.map(x=> <span onChange={myChange(this)} style={{width:500/myIndex,height:500/myIndex}} key={x.toString}></span>)
   }
   </div>
 );
