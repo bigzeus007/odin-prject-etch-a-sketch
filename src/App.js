d1d25df4c8ca0b2e1ff5dac9446c20bf6349e0e3
said.T <tarhisaid@gmail.com>
2021-06-14 00:10:03 +0100

    React learing
    learning

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import {useState} from 'react'
-import './App.css';
-import MyPixel from './components/MyPixel'
+import MyPixel from './components/MyPixel';
 
 
 
 
 function App(props) {
-  const myInput= document.querySelector('.myInput');
-  const mySize = 4;
+  const myValue=document.getElementsByTagName('input');
+  
+  const [mySize,setSize] = useState(4);
+
+  function changeSize(x){
+    
+    setSize(x);
+
+  }
+
+
+  
   
  
   return (
     <div className="App">
       <header className="App-header" >
-        <input className='myInput' defaultValue={mySize}></input>
-        <button className="mySetButton"style={{width:50, height:50}}></button>
+      <div className="monTab" style={{width:500, height:550}}>
+        <input type='number' min={0} max={10} className='myInput' style={{height:50, width:250}} defaultValue={4}></input>
+        <button onClick={()=>console.log(myValue)} className="SetSize" style={{height:55, width:200,backgroundColor:'green'}} ></button>
         <MyPixel mySize={mySize}></MyPixel>
- 
+        </div>
       </header>
     </div>
   );
--- src/components/MyPixel.js
+++ src/components/MyPixel.js
@@ -1,9 +1,26 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import '../App.css';
 
-const MyPixel = ({mySize})=>{
+function MyPixel({mySize}){
+    const myArray=[]
+    myArray.length=Math.pow(mySize,2)
+    for (let index = 0; index < myArray.length; index++) {
+        myArray[index]=index;
+        
+    }
+    
+    
 
 
-return(<span children={mySize*2}></span>);
+
+return(
+
+
+    myArray.map((element)=>
+    <button className='myCell' key={element.toString()} className={"monTableau"} style={{ width:500/mySize, height:500/mySize}}></button>)
+
+)
 }
 
 export default MyPixel;
