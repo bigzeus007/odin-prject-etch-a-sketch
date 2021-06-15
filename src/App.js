7062fbc59bf37568b6d65295af429c7c7c1f4223
said.T <tarhisaid@gmail.com>
2021-06-15 20:25:39 +0100

    React stacking
    training

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import {useState} from 'react'
 import MyPixel from './components/MyPixel';
 
 
-
+//
 
 function App() {
  
-  
   const [mySize,setSize] = useState(4);
-
 
   return (
     <div className="App">
       <header className="App-header" >
       <div className="monTab" style={{width:500, height:550}}>
         <input id="setMyInputValue" type='number' min={0} max={100} className='myInput' style={{height:50, width:250}} defaultValue={mySize}></input>
-        <button   onClick={()=>setSize(document.getElementById('setMyInputValue').value)} className="SetSize" style={{height:55, width:240,backgroundColor:'green'}} ></button>
+        <button   onClick={()=>setSize(document.getElementById('setMyInputValue').value)}  className="SetSize" style={{height:55, width:240,backgroundColor:'green'}} ></button>
         <MyPixel mySize={mySize}></MyPixel>
         </div>
       </header>
--- src/components/MyPixel.js
+++ src/components/MyPixel.js
@@ -1,24 +1,41 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { useState } from 'react';
 import '../App.css';
 
 function MyPixel({mySize}){
+
+
+   
+    
+    const [myStart,setStart]=useState(false)
+
+    
+
+ 
+    function mySetHover(){
+    myStart===false? setStart(true):setStart(false)
+    }
+    const myChoosenColor=[]
+    function myHover(element) {
+        element.myChoosenColor='black'
+
+    }
+  
+
     const myArray=[]
     myArray.length=Math.pow(mySize,2)
     for (let index = 0; index < myArray.length; index++) {
-        myArray[index]=index;
+        myArray[index]={ 
+            index:index,
+            myChoosenColor: myChoosenColor[index]
+        };
         
     }
-    
-    
-
-
 
 return(
-
-
     myArray.map((element)=>
-    <button className='myCell' key={element.toString()} className={"monTableau"} style={{height:500/mySize,width:500/mySize}}/>)
+    <button onMouseOver={()=>{myHover(element);console.log(element);}} className='myCell' key={element.toString()} className={"monTableau"} style={{height:500/mySize,width:500/mySize,backgroundColor:element.myChoosenColor}}/>)
 
 )
 }
