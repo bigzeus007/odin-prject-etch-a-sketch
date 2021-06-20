213de1923501be313b7b3253b12badf1462034ea
said.T <tarhisaid@gmail.com>
2021-06-20 23:17:27 +0100

    React sketch
    Styling

--- src/App.js
+++ src/App.js
@@ -16,9 +16,10 @@ function App() {
       <header className="App-header" >
       <div className="monTab" style={{width:500, height:550}}>
         <input id="setMyInputValue" type='number' min={0} max={100} className='myInput' style={{height:50, width:250}} defaultValue={mySize}></input>
-        <button   onClick={()=>setSize(document.getElementById('setMyInputValue').value)}  className="SetSize" style={{height:55, width:240,backgroundColor:'lime'}} />
+        <button   onClick={()=>setSize(document.getElementById('setMyInputValue').value)}  className="SetSize" style={{height:55, width:240,backgroundColor:'lime'}} children="Dimension" />
         <MyPixel mySize={mySize} myOtherColor={myOtherColor}></MyPixel>
-        <input onChange={(e)=>{setMyOtherColor(e.target.value);console.log(myOtherColor)}} id="myColor" type="color" defaultValue="#ff0000"/>
+        <input onChange={(e)=>{setMyOtherColor(e.target.value)}} id="myColor" type="color" style={{height:55,width:400}} defaultValue="#ff0000"/>
+        <label htmlFor="myColor"  style={{color:"yellow",backgroundColor:myOtherColor,width:"400px"}} children="Couleurs"/>
         </div>
       </header>
     </div>
